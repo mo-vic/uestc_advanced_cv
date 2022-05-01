f57c4ab5a91258a73fe30b502940d2979fbdb797
movic <movic.chen@gmail.com>
2022-05-01 12:39:32 +0800

    添加Proj4实现
    提取HoG特征时设置`cells_per_block = (1, 1)`。
    在检测时需要重新排列HoG特征，实现比较麻烦。

---
+++ Proj4/readme.md
@@ -0,0 +1,113 @@
+## 说明
+
+仓库提供了Proj4的两种实现：
+
+1. `cells_per_block = (1, 1)`
+2. `cells_per_block = (n_cell, n_cell)`
+
+这两种实现方式得到的`HoG`特征模板的维数一样，但是归一化方式不一样。
+
+在方式1中，由于每个`block`里面只包含一个`cell`，`skimage.feature.hog`函数会对每个`cell`单独进行归一化，每个`cell`的HoG特征为单位向量，`HoG`特征模板由多个`cell`拼接得到，其不一定为单位向量。
+
+在方式2中，由于一个`block`包含了`HoG`模板中的所有`cell`，所以`skimage.feature.hog`函数的返回结果只有一个`block`，且为单位向量。
+
+由于`skimage.feature.hog`函数的返回结果是以`block`为单位，并按照先行后列的顺序进行排列的，对于方式1，需要对返回结果用特定的索引进行重新排列，才能构造正确的模板参数。重新排列的时候可以使用双重`for`循环实现滑动窗口操作，但是速度太慢。这里提供了矩阵化的操作进行重排列。
+
+实验过程中发现方式2的性能比方式1更好一些。但是用了挺长时间才写完方式1的代码，还是作为一个提交版本保存下来吧。你可以通过提交历史切换不同的版本。
+
+
+
+
+
+## 参数设置
+
+* 设置小的`hog_cell_size`可以提高检测性能，但是运行速度会降低很多
+* `orientations`与`HoG`特征的维度相关，调整`orientations`参数时，会导致SVM模型的参数发生变化，可以适当修改SVM的正则化参数`C`
+
+
+
+### 方式1
+
+```python
+# HoG template
+feature_params = {"template_size": 36, "hog_cell_size": 3, "orientations" : 31}
+
+# training data
+num_negative_examples = 10000
+
+# svm 1
+C = 5e-2
+class_weight={1:10}
+## confidence threshold
+threshold = 0.5
+topk = 200
+
+# svm 2
+C = 5e-2
+class_weight={1:10}
+## confidence threshold
+threshold = 0.1
+topk = 200
+```
+
+**AP**：
+
+`SVM_1`：$\approx 0.723$
+
+`SVM_2`：$\approx 0.361$
+
+（我已经调不动了，电脑扛不住了）
+
+
+
+### 方式2
+
+```python
+# HoG template
+feature_params = {"template_size": 36, "hog_cell_size": 3, "orientations" : 31}
+
+# training data
+num_negative_examples = 10000
+
+# svm 1
+C = 1.0
+class_weight={1:10}
+## confidence threshold
+threshold = 0.5
+topk = 500
+
+
+# svm 2
+C = 1.0
+class_weight={1:10}
+## confidence threshold
+threshold = 0.1
+topk = 500
+```
+
+**AP**：
+
+`SVM_1`：$\approx 0.761$
+
+`SVM_2`：$\approx 0.529$
+
+（我已经调不动了，电脑扛不住了）
+
+
+
+## 调参心得
+
+* 设置小的`hog_cell_size`
+
+* `orientations`参数似乎影响不太大
+
+* 设置SVM模型中的类别权重，使得假阳性率稍高一些
+
+* 先调整SVM模型的参数，至少在训练集上看得过去，再调整其他参数
+
+* `SVM_2`的参数太难调了，而且`hard_negative`样本的生成太耗时了，等不起
+
+* 我人麻了。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。
+
+  
+
--- Proj4/student.py
+++ Proj4/student.py
@@ -1,10 +1,12 @@
-import numpy as np
-from skimage.feature import hog
 from utils import *
 import os.path as osp
 from glob import glob
-from random import shuffle
+from functools import partial
+
+import numpy as np
+from skimage.feature import hog
 from sklearn.svm import LinearSVC
+from skimage.transform import rescale
 
 
 def get_positive_features(train_path_pos, feature_params):
@@ -42,25 +44,35 @@ def get_positive_features(train_path_pos, feature_params):
             feature_params['hog_cell_size'])^2 * 31 if you're using the default
             hog parameters.
     """
-    # params for HOG computation
+
+    # Params for HOG computation
     win_size = feature_params.get('template_size', 36)
     cell_size = feature_params.get('hog_cell_size', 6)
+    orientations = feature_params.get('orientations', 31)
 
     positive_files = glob(osp.join(train_path_pos, '*.jpg'))
 
     ###########################################################################
     #                           TODO: YOUR CODE HERE                          #
     ###########################################################################
-    feats = None
 
+    n_cell = win_size // cell_size
 
-
+    """
+    In practice, cells_per_block = (1, 1) works better than cell_per_block = (n_cell, n_cell). However, setting 
+    cells_per_block = (1, 1) makes detection or mining hard example a bit cumbersome. Make sure to re-arrange 
+    the HoG features that matches the structure of the template if you set cells_per_block = (1, 1). And I found 
+    cells_per_block = (1, 1) works better only in terms of training accuracy, the testing performance is terrible.
+    """
+    f = lambda x : hog(load_image_gray(x), orientations=orientations, pixels_per_cell=(cell_size, cell_size), cells_per_block=(1, 1), feature_vector=True).reshape((-1, n_cell ** 2 * orientations))
+    feats = np.concatenate(list(map(f, positive_files)))
 
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
 
     return feats
+
 
 def get_random_negative_features(non_face_scn_path, feature_params, num_samples):
     """
@@ -92,23 +104,47 @@ def get_random_negative_features(non_face_scn_path, feature_params, num_samples)
             feature_params['hog_cell_size'])^2 * 31 if you're using the default
             hog parameters.
     """
-    # params for HOG computation
+
+    # Params for HOG computation
     win_size = feature_params.get('template_size', 36)
     cell_size = feature_params.get('hog_cell_size', 6)
+    orientations = feature_params.get('orientations', 31)
 
     negative_files = glob(osp.join(non_face_scn_path, '*.jpg'))
 
     ###########################################################################
     #                           TODO: YOUR CODE HERE                          #
     ###########################################################################
-    feats = None
+
+    n_cell = win_size // cell_size
+    # Number of patches to be sampled from each image
+    num_per_img = num_samples // len(negative_files)
+    # Compute the rest number of patches
+    residual = num_samples % len(negative_files)
+
+    num_per_img = np.full(len(negative_files), num_per_img)
+    # Randomly dispatch the residual
+    num_per_img[np.random.permutation(len(negative_files))[:residual]] += 1
+
+    """
+    In practice, cells_per_block = (1, 1) works better than cell_per_block = (n_cell, n_cell). However, setting 
+    cells_per_block = (1, 1) makes detection or mining hard example a bit cumbersome. Make sure to re-arrange 
+    the HoG features that matches the structure of the template if you set cells_per_block = (1, 1). And I found 
+    cells_per_block = (1, 1) works better only in terms of training accuracy, the testing performance is terrible.
+    """
+    f = lambda x : (lambda im=load_image_gray(x[0]) : [hog(im[start:start+win_size, stop:stop+win_size], orientations=orientations, pixels_per_cell=(cell_size, cell_size), cells_per_block=(1, 1), feature_vector=True).reshape((-1, n_cell ** 2 * orientations))
+    for start, stop in zip(np.random.randint(0, im.shape[0] - win_size, x[1]), np.random.randint(0, im.shape[1] - win_size, x[1]))])()
+
+    feats = np.concatenate(list(map(f, zip(negative_files, num_per_img)))).squeeze(1)
+
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
 
     return feats
 
-def train_classifier(features_pos, features_neg, C):
+
+def train_classifier(features_pos, features_neg, C, class_weight={1:10}):
     """
     This function trains a linear SVM classifier on the positive and negative
     features obtained from the previous steps. We fit a model to the features
@@ -127,15 +163,93 @@ def train_classifier(features_pos, features_neg, C):
     ###########################################################################
     #                           TODO: YOUR CODE HERE                          #
     ###########################################################################
-    svm = None
 
+    N, D = features_pos.shape
+    M, D = features_neg.shape
 
+    train_feats = np.vstack([features_pos, features_neg])
+    train_labels = np.hstack([np.ones(N), -np.ones(M)])
+
+    svm = LinearSVC(max_iter=10000, C=C, class_weight=class_weight)
+    svm.fit(train_feats, train_labels)
 
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
 
     return svm
+
+
+def slide2d(array, win_size, stride=1):
+    """
+    2D sliding window operation.
+    Used to convert input array to patches.
+    """
+
+    m, n, n_cells_row, n_cells_col, n_orient = array.shape
+
+    # Works for stride == 1 for now
+    assert stride == 1
+
+    # Number of blocks in row
+    n_blocks_row = n - win_size + 1
+    # Number of blocks in column
+    n_blocks_col = m - win_size + 1
+
+    # Index in each dimension
+    i = np.repeat(np.arange(0, win_size).reshape((1, -1)), n_blocks_col, axis=0)
+    j = np.repeat(np.arange(0, win_size).reshape((1, -1)), n_blocks_row, axis=0)
+    i = i + np.arange(0, n_blocks_col).reshape((n_blocks_col, 1))
+    j = j + np.arange(0, n_blocks_row).reshape((n_blocks_row, 1))
+
+    # Broadcasting
+    j = np.tile(np.tile(j, win_size), [n_blocks_col, 1]).flatten()
+    i = np.tile(np.repeat(i, win_size, axis=1), n_blocks_row).flatten()
+
+    # Retrieve the element
+    blocks = np.array(array[i, j])
+
+    blocks = blocks.reshape((n_blocks_col, n_blocks_row, win_size * win_size, n_cells_row, n_cells_col, n_orient))
+    # print("Shape of blocks", blocks.shape)
+
+    # # Identity check
+    # check = []
+    # for i in range(0, n_blocks_col):
+    #     for j in range(0, n_blocks_row):
+    #         check.append(np.all(array[i:i+win_size, j:j+win_size].reshape((win_size*win_size, n_cells_row, n_cells_col, n_orient)) == blocks[i, j]))
+    # print("All check:", np.all(check))
+
+    return blocks
+
+
+
+def mining(file_path, svm, win_size, cell_size, orientations):
+    # Loading image
+    image = load_image_gray(file_path)
+
+    # Extract HoG features
+    hog_feats = hog(image, orientations=orientations, pixels_per_cell=(cell_size, cell_size), cells_per_block=(1, 1), feature_vector=False)
+
+    # Group HoG features into blocks by sliding over `hog_feats`
+    n_cell = win_size // cell_size
+    hog_feats = slide2d(hog_feats, win_size=n_cell)
+
+    # Reshape to (`num_blocks`, -1), here `num_blocks` matches win_size, i.e., the structure of the template
+    n_blocks_col, n_blocks_row =  hog_feats.shape[:2]
+    hog_feats = hog_feats.reshape((n_blocks_col * n_blocks_row, -1))
+
+    # Make predictions on all blocks
+    predictions = svm.predict(hog_feats)
+    # Pick the HoG features that predicted as false positive to be our hard negative examples
+    false_positives = hog_feats[predictions == 1]
+
+    # # Compute confidence for all blocks
+    # confidences = svm.decision_function(hog_feats)
+    # # Pick the HoG features that predicted as false positive to be our hard negative examples
+    # false_positives = hog_feats[np.logical_and(0 <= confidences, confidences <= 0.01)]
+
+    return false_positives
+
 
 def mine_hard_negs(non_face_scn_path, svm, feature_params):
     """
@@ -162,16 +276,18 @@ def mine_hard_negs(non_face_scn_path, svm, feature_params):
             false-positive and D is the feature dimensionality.
     """
 
-    # params for HOG computation
+    # Params for HOG computation
     win_size = feature_params.get('template_size', 36)
     cell_size = feature_params.get('hog_cell_size', 6)
+    orientations = feature_params.get('orientations', 31)
 
     negative_files = glob(osp.join(non_face_scn_path, '*.jpg'))
 
     ###########################################################################
     #                           TODO: YOUR CODE HERE                          #
     ###########################################################################
-    feats = None
+
+    feats = np.concatenate(list(map(partial(mining, svm=svm, win_size=win_size, cell_size=cell_size, orientations=orientations), negative_files)))
     
     ###########################################################################
     #                             END OF YOUR CODE                            #
@@ -179,7 +295,8 @@ def mine_hard_negs(non_face_scn_path, svm, feature_params):
 
     return feats
 
-def run_detector(test_scn_path, svm, feature_params, verbose=False):
+
+def run_detector(test_scn_path, svm, feature_params, threshold=0.5, verbose=False):
     """
     This function returns detections on all of the images in a given path. You
     will want to use non-maximum suppression on your detections or your
@@ -220,7 +337,7 @@ def run_detector(test_scn_path, svm, feature_params, verbose=False):
             -   hog_cell_size: (default 6) The number of pixels in each HoG
             cell. template size should be evenly divisible by hog_cell_size.
             Smaller HoG cell sizes tend to work better, but they make things
-            slowerbecause the feature dimensionality increases and more
+            slower because the feature dimensionality increases and more
             importantly the step size of the classifier decreases at test time.
     -   verbose: prints out debug information if True
 
@@ -232,26 +349,35 @@ def run_detector(test_scn_path, svm, feature_params, verbose=False):
     -   image_ids: List with N elements. image_ids[i] is the image file name
             for detection i. (not the full path, just 'albert.jpg')
     """
-    im_filenames = sorted(glob(osp.join(test_scn_path, '*.jpg')))
-    bboxes = np.empty((0, 4))
-    confidences = np.empty(0)
+
+    bboxes = []
     image_ids = []
+    confidences = []
+    im_filenames = sorted(glob(osp.join(test_scn_path, '*.jpg')))
 
-    # number of top detections to feed to NMS
-    topk = 40
+    # Number of top detections to feed to NMS
+    topk = 200
 
-    # params for HOG computation
+    # Confidence threshold
+    thres = threshold
+
+    # Params for HOG computation
     win_size = feature_params.get('template_size', 36)
     cell_size = feature_params.get('hog_cell_size', 6)
-    scale_factor = feature_params.get('scale_factor', 0.65)
-    template_size = int(win_size / cell_size)
+    orientations = feature_params.get('orientations', 31)
+
+    n_cell = win_size // cell_size
+    multi_scale_factor = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
 
     for idx, im_filename in enumerate(im_filenames):
         print('Detecting faces in {:s}'.format(im_filename))
         im = load_image_gray(im_filename)
         im_id = osp.split(im_filename)[-1]
         im_shape = im.shape
-        
+
+        bboxes_per_image = []
+        confidences_per_image = []
+
         #######################################################################
         #                        TODO: YOUR CODE HERE                         #
         #######################################################################
@@ -265,7 +391,7 @@ def run_detector(test_scn_path, svm, feature_params, verbose=False):
         #           xxx
         # 3. image to hog feature
         # 4. sliding windows at scaled feature map. you can use horizontally 
-        #    recurrence and and vertically recurrence
+        #    recurrence and vertically recurrence
         # 5. extract feature for current bounding box and use classify
         # 6. record your result for this image
         # 7. non-maximum suppression (NMS)
@@ -280,16 +406,68 @@ def run_detector(test_scn_path, svm, feature_params, verbose=False):
         #     is_valid_bbox = non_max_suppression_bbox(cur_bboxes, cur_confidences,
         #        im_shape, verbose=verbose)
 
+        for scale_rate in multi_scale_factor:
+            scaled_im = rescale(im, scale=scale_rate, anti_aliasing=True)
+            hog_feats = hog(scaled_im, orientations=orientations, pixels_per_cell=(cell_size, cell_size), cells_per_block=(1, 1), feature_vector=False)
+
+            n_blocks_col, n_blocks_row =  hog_feats.shape[:2]
+            if (n_blocks_col // n_cell) * (n_blocks_row // n_cell) == 0:
+                break
+
+            hog_feats = slide2d(hog_feats, win_size=n_cell)
+
+            # Reshape to (`num_blocks`, -1), here `num_blocks` matches win_size, i.e., the structure of the template
+            n_blocks_col, n_blocks_row =  hog_feats.shape[:2]
+            hog_feats = hog_feats.reshape((n_blocks_col * n_blocks_row, -1))
+
+            # Compute confidence on all blocks
+            confidences_per_scale = svm.decision_function(hog_feats)
+
+            # Reshape confidences to (n_blocks_col, n_blocks_row)
+            confidences_per_scale = confidences_per_scale.reshape((n_blocks_col, n_blocks_row))
 
 
+            detected_faces = (confidences_per_scale >= thres)
+            confidences_per_scale = confidences_per_scale[detected_faces].flatten()
 
+            if confidences_per_scale.size == 0:
+                continue
 
+            (block_ys, block_xs) = np.where(detected_faces)
+            x_min = block_xs * cell_size
+            y_min = block_ys * cell_size
+            x_max = block_xs * cell_size + win_size
+            y_max = block_ys * cell_size + win_size
+
+            bboxes_per_scale = np.array([x_min, y_min, x_max, y_max]).T
+            bboxes_per_scale = bboxes_per_scale / scale_rate
+            bboxes_per_image.append(bboxes_per_scale)
+            confidences_per_image.append(confidences_per_scale)
+
+        if len(bboxes_per_image) == 0:
+            continue
+
+        bboxes_per_image = np.concatenate(bboxes_per_image, axis=0).astype(np.int32)
+        confidences_per_image = np.hstack(confidences_per_image)
+
+        sorted_index = np.argsort(confidences_per_image)[::-1]
+        bboxes_per_image = bboxes_per_image[sorted_index[:topk]]
+        confidences_per_image = confidences_per_image[sorted_index[:topk]]
+        survives = non_max_suppression_bbox(bboxes_per_image, confidences_per_image, im_shape, verbose=verbose)
+
+        bboxes_per_image = bboxes_per_image[survives]
+        confidences_per_image = confidences_per_image[survives]
+
+        bboxes.append(bboxes_per_image)
+        confidences.append(confidences_per_image)
+        image_ids.extend([im_id] * np.sum(survives))
+
+    bboxes = np.concatenate(bboxes, axis=0)
+    confidences = np.hstack(confidences)
+    image_ids = np.array(image_ids)
 
         #######################################################################
         #                          END OF YOUR CODE                           #
         #######################################################################
-
-        
-
 
     return bboxes, confidences, image_ids
